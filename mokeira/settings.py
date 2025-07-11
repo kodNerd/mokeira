19178011b6b04cb022fab12368608ba2420f5425
kodNerd <bencoder9@gmail.com>
2025-07-11 11:17:18 -0700

    mokeira

--- mokeira/settings.py
+++ mokeira/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'pos',
     'widget_tweaks',
     'social_django',
 ]
